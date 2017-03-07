5f3c70d7134d1d210f7e42d0c797fd2abc25d30a
Viswada Yangala <yangalav@go.stockton.edu>
2017-03-07 17:49:55 -0500

    Attempted to connect database to heroku again and again

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var app = express();
-var mongojs = require('./db/config.js');
-var db = mongojs('contactlist', ['contactlist']);
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']); 
 var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public" ));
