b76d3ec9e8f013726228053ed1bfccbf9ab2f45e
Viswada Yangala <yangalav@go.stockton.edu>
2017-03-07 15:42:58 -0500

    Edited

--- server.js
+++ server.js
@@ -49,5 +49,5 @@ app.put('/contactlist/:id', function(req, res){
     });
 });
 
-app.listen(3000);
-console.log("Server running on port 3000");
+app.listen(process.env.PORT || 3000);
+console.log("Server running on port" + process.env.PORT);
